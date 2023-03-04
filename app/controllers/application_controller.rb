44fe4b00a46fa7a8abcf5d2cf2e52c237b7f22a8
Stellanyambura <stellagitonga.com>
2023-03-04 09:28:27 +0300

    fixed bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,8 @@
-
 require 'sinatra'
- require 'json'
-class ApplicationController < Sinatra::Base
-
-  set :default_content_type, 'application/json'
-
-  enable :sessions
+require 'json'
 
 # Define a route to get all projects for a user
-
-get '/users' do
-users = User.all
-users.to_json(include: [:projects, :skills])
-  end
-
+class ApplicationController < Sinatra::Base
 get '/users/:id/projects' do
   content_type :json
   user_id = params[:id]
@@ -22,45 +11,27 @@ get '/users/:id/projects' do
   # Return the projects as JSON
   projects.to_json
 end
-
 # create a new user in the database
-post 'https://phase-three-sinatra-project.onrender.com/add/user' do
+post '/add/user' do
   # check if email already exists in database
   if User.find_by(email: params[:email])
     return "Error: Email already exists"
   end
-  # create a new user
-  user = User.new(
-    first_name: params[:first_name],
-    last_name: params[:last_name],
-    email: params[:email],
-    career: params[:career],
-    bio: params[:bio],
-    password: params[:password]
-  )
-  user.save
-end
-
-# login
-  # Handle login form submission
- post "/login" do
-  user = User.find_by(email: params[:email])
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user[:id]
-    content_type :json
-    { success: true, message: "Login successful" }.to_json
+# Define a route to create a new project for a user
+post '/users/:id/projects' do
+  content_type :json
+  user_id = params[:id]
+  project = Project.new(params[:project])
+  project.user_id = user_id
+  # Save the project to the database
+  if project.save
+    # Return the new project as JSON
+    project.to_json
   else
-    content_type :json
-    status 401 # unauthorized
-    { error: "Invalid email or password." }.to_json 
+    # Return an error message as JSON
+    { error: 'Failed to create project' }.to_json
   end
- end
-
-  # user's dashboard
-  get "/user/:email" do
-  user = User.find_by(email: params[:email])
-  user.to_json(include: [:projects, :skills])
-  end
+end
 
 # Define a route to update an existing project for a user
 put '/users/:id/projects/:project_id' do
@@ -94,115 +65,5 @@ delete '/users/:id/projects/:project_id' do
     # Return an error message as JSON
     { error: 'Project not found' }.to_json
   end
-  end
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# require 'sinatra'
-# require 'json'
-
-# # Define a route to get all projects for a user
-# class ApplicationController < Sinatra::Base
-# get '/users/:id/projects' do
-#   content_type :json
-#   user_id = params[:id]
-#   # Retrieve all projects for the given user from the database
-#   projects = Project.where(user_id: user_id)
-#   # Return the projects as JSON
-#   projects.to_json
-# end
-
-# # Define a route to create a new project for a user
-# post '/users/:id/projects' do
-#   content_type :json
-#   user_id = params[:id]
-#   project = Project.new(params[:project])
-#   project.user_id = user_id
-#   # Save the project to the database
-#   if project.save
-#     # Return the new project as JSON
-#     project.to_json
-#   else
-#     # Return an error message as JSON
-#     { error: 'Failed to create project' }.to_json
-#   end
-# end
-
-# # Define a route to update an existing project for a user
-# put '/users/:id/projects/:project_id' do
-#   content_type :json
-#   user_id = params[:id]
-#   project_id = params[:project_id]
-#   project = Project.where(user_id: user_id, id: project_id).first
-#   if project
-#     # Update the project attributes
-#     project.update_attributes(params[:project])
-#     # Return the updated project as JSON
-#     project.to_json
-#   else
-#     # Return an error message as JSON
-#     { error: 'Project not found' }.to_json
-#   end
-# end
-
-# # Define a route to delete a project for a user
-# delete '/users/:id/projects/:project_id' do
-#   content_type :json
-#   user_id = params[:id]
-#   project_id = params[:project_id]
-#   project = Project.where(user_id: user_id, id: project_id).first
-#   if project
-#     # Delete the project from the database
-#     project.destroy
-#     # Return a success message as JSON
-#     { message: 'Project deleted successfully' }.to_json
-#   else
-#     # Return an error message as JSON
-#     { error: 'Project not found' }.to_json
-#   end
-# end
-# end
+end
