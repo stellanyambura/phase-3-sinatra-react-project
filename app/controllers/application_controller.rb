4578d5b800ff18a93fffc2b6b20b88f0b28898ff
Stellanyambura <stellagitonga.com>
2023-03-05 19:02:54 +0300

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,66 @@ post '/add/user' do
     email: params[:email],
     password_digest: params[:password_digest]
   )
-end
 
 end
+
+ # Define a route to create a new project for a user
+ post '/users/:id/projects' do
+   content_type :json
+   user_id = params[:id]
+   project = Project.new(params[:project])
+   project.user_id = user_id
+ end
+  # Save the project to the database
+  if project.save
+    # Return the new project as JSON
+     project.to_json
+  else
+         # Return an error message as JSON
+     { error: 'Failed to create project' }.to_json
+  end
+
+ # Define a route to create a new project for a user
+post '/users/:id/projects' do
+  content_type :json
+    user_id = params[:id]
+  project = Project.new(params[:project])
+     project.user_id = user_id
+  # Save the project to the database
+  if project.save
+     Return the new project as JSON
+    project.to_json
+  else
+      Return an error message as JSON
+     { error: 'Failed to create project' }.to_json
+   end
+ # Define a route to update an existing project for a user
+put '/users/:id/projects/:project_id' do
+  content_type :json
+  user_id = params[:id]
+   project_id = params[:project_id]
+   project = Project.where(user_id: user_id, id: project_id).first
+   if project
+     # Update the project attributes
+     project.update_attributes(params[:project])
+          # Return the updated project as JSON
+    project.to_json
+   else
+     # Return an error message as JSON
+     { error: 'Project not found' }.to_json
+   end
+ 
+# Define a route to delete a project for a user
+ delete '/users/:id/projects/:project_id' do
+   content_type :json#   user_id = params[:id]
+   project_id = params[:project_id]
+   project = Project.where(user_id: user_id, id: project_id).first
+   if project
+     # Delete the project from the database
+     project.destroy
+     # Return a success message as JSON
+    { message: 'Project deleted successfully' }.to_json
+   else
+    # Return an error message as JSON
+    { error: 'Project not found' }.to_json
+   end
